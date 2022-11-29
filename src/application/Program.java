1bc4ada6840ad684cca4662050d2e849b425d980
Raphael Viero <raphaviero@gmail.com>
2022-11-29 11:49:24 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		//teste de funcionalidade classe department
+		
+		Department obj = new Department(1,"Books");
+		
+		System.out.println(obj);
+		
+	}
+
+}
