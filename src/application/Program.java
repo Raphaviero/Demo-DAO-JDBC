5dd65e7e0d95527d71d2547dc82c68d3bab6bf72
Raphael Viero <raphaviero@gmail.com>
2022-11-29 11:58:18 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -12,6 +15,8 @@ public class Program {
 		
 		System.out.println(obj);
 		
+		Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
 	}
 
 }
